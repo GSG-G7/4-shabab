5033b781192cdb3fb19538ad626d7f78070b3f73
fares98 <faresalhello21@gmail.com>
2019-09-23 09:27:45 +0300

    finish the getServices function Reltes #37

--- server/db/queries/getServices.js
+++ server/db/queries/getServices.js
@@ -1,9 +1,10 @@
 const connection = require('./../config/connection');
 
 const getServices = () => {
-  connection.query('select name, imgUrl(if exist) from services')
+  connection.query('select name, imgUrl from services')
     .then(console.log('Done'))
     .catch((err) => console.log(err));
 };
+
 
 module.exports = getServices;
