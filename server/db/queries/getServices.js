85361ec813ec859e48e8cf02bc6c0b8416fc7ef1
fares98 <faresalhello21@gmail.com>
2019-09-23 09:11:08 +0300

    handle the error Relates #37

--- server/db/queries/getServices.js
+++ server/db/queries/getServices.js
@@ -1,10 +1,9 @@
 const connection = require('./../config/connection');
 
 const getServices = () => {
-  connection.query(
-    'select name from services',
-    // 'select name, imgUrl(if exist) from services',
-  );
+  connection.query('select name, imgUrl(if exist) from services')
+    .then(console.log('Done'))
+    .catch((err) => console.log(err));
 };
 
 module.exports = getServices;
