9d068fcdaab6b0d035a26315bd324c646130cdbd
fares98 <faresalhello21@gmail.com>
2019-09-24 12:06:52 +0300

    edit on the test function for query Relates #37

--- server/tests/queries/getServices.js
+++ server/tests/queries/getServices.js
@@ -2,10 +2,8 @@ const test = require('tape');
 const { getServices } = require('../../db/queries/getServices');
 
 exports.getServices = test('testing for getServices', async (t) => {
-  const expected = true;
   const obj = await getServices();
   const actual = !!(obj.rows.length);
-
-  t.equal(actual, expected, 'TESTING services');
+  t.equal(actual, true, 'TESTING services');
   t.end();
 });
