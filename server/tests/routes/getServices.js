5b70456fcf5a75aad238a357b19ebcbbac7c37d8
fares98 <faresalhello21@gmail.com>
2019-09-24 14:02:52 +0300

    remove the async await Relates #37

--- server/tests/routes/getServices.js
+++ server/tests/routes/getServices.js
@@ -1,23 +1,24 @@
 const test = require('tape');
 const supertest = require('supertest');
 
-const { routeServices } = require('../../controllers/routes/getServices');
 const app = require('./../../app');
-test('Status Code equal 200', async (t) => {
 
-  try {
-    supertest(app)
-      .get('/api/v1/services')
-      .expect(00)
-      .expect('content-type', /json/)
-      .end((err, res) => {
+test('Status Code equal 200', (t) => {
+  supertest(app)
+    .get('/api/v1/services')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        t.error(err);
+        t.end();
+      } else {
         const actual = !!(res.body.data.length);
         t.deepEqual(actual, true, 'Number of items should not equal zero');
         t.end();
-      });
-  } catch (err) {
-    console.log('err', err);
-  }
+      }
+    });
+
 });
 
 test.onFinish(() => process.exit(0));
